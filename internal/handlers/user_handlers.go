04ec298a4b2e494abe2439e95fc1f6e017f9573e
andrenormanlang <andrernormanlang@gmail.com>
2024-07-29 14:51:38 +0200

    added credentials as .env

--- internal/handlers/user_handlers.go
+++ internal/handlers/user_handlers.go
@@ -205,32 +205,6 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(user)
 }
 
-// const (
-//     googleProjectID   = "blogflex-images"
-//     googleBucketName  = "images-blogs"
-//     googleCredentials = "./credentials/blogflex-images-d00c068cf344.json" // Update this path
-// )
-
-// func UploadFileToGCS(file multipart.File, fileName string) (string, error) {
-//     ctx := context.Background()
-
-//     client, err := storage.NewClient(ctx, option.WithCredentialsFile(googleCredentials))
-//     if err != nil {
-//         return "", fmt.Errorf("storage.NewClient: %v", err)
-//     }
-//     defer client.Close()
-
-//     wc := client.Bucket(googleBucketName).Object(fileName).NewWriter(ctx)
-//     if _, err = io.Copy(wc, file); err != nil {
-//         return "", fmt.Errorf("io.Copy: %v", err)
-//     }
-//     if err := wc.Close(); err != nil {
-//         return "", fmt.Errorf("Writer.Close: %v", err)
-//     }
-
-//     return fmt.Sprintf("https://storage.googleapis.com/%s/%s", googleBucketName, fileName), nil
-// }
-
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
     var user models.User
     var blogName, blogDescription, blogImagePath string
